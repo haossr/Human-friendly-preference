29fe347db51bcabf4372632551b4a522a392a589
Hao <haosheng@stanford.edu>
2020-03-02 13:11:10 -0800

    update of examples

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python main.py simulate --iteration 100 --M [1,2,3,4,5,10,20,30,50,80,100,150,200] --B 60 
+python main.py simulate --iteration 50 --M [1,2,3,4,5,10,20,30,50,80,100,150,200] --B 100 
 python main.py plot
